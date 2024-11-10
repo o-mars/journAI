1641eb836e88d2c7f284b0c4fa50637818a752f5
Omar Sherwani <ohm.mars@protonmail.com>
2024-11-10 16:54:07 -0500

    get angular build to work on render

---
+++ angular/generate-environment.ts
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const serverUrl = process.env['REACT_APP_LOCAL_RELAY_SERVER_URL'];
+const environmentFileContent = `
+export const environment = {
+   production: true,
+   relayServerUrl: 'ws://${serverUrl}',
+   serverUrl: '${serverUrl}:8081',
+};
+`;
+fs.writeFileSync('./src/environment.ts', environmentFileContent);
